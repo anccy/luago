0850d4440943cf64f43c009c3fce3a573a61e401
anccy <y980715@gmail.com>
2022-12-21 00:30:52 +0800

    feat: add chunk struct

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anccy/luago
+
+go 1.18
---
+++ go/binchunk/binary_chunk.go
@@ -0,0 +1,24 @@
+package binchunk
+
+type binChunk struct {
+	header
+	sizeUpvalues byte
+	mainFunc     *Prototype
+}
+
+type header struct {
+	signature       [4]byte
+	version         byte
+	format          byte
+	luacData        [6]byte
+	cintSize        byte
+	sizetSize       byte
+	instructionSize byte
+	luaIntegerSize  byte
+	luaNumberSize   byte
+	luacInt         int64
+	luacNum         float64
+}
+
+type Prototype struct {
+}
