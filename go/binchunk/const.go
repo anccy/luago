58678dd5ee0e0360e35ac738a022869e21de6733
anccy <y980715@gmail.com>
2023-02-07 00:20:17 +0800

    feat: add const

---
+++ go/binchunk/const.go
@@ -0,0 +1,21 @@
+package binchunk
+
+const (
+	LUA_VERSION_MAJOR   = "5"
+	LUA_VERSION_MINOR   = "3"
+	LUA_VERSION_NUM     = 503
+	LUA_VERSION_RELEASE = "6"
+
+	LUA_VERSION   = "Lua " + LUA_VERSION_MAJOR + "." + LUA_VERSION_MINOR
+	LUA_RELEASE   = LUA_VERSION + "." + LUA_VERSION_RELEASE
+	LUA_COPYRIGHT = LUA_RELEASE + "  Copyright (C) 1994-2020 Lua.org, PUC-Rio"
+	LUA_AUTHORS   = "R. Ierusalimschy, L. H. de Figueiredo, W. Celes"
+
+	LUA_SIGNATURE = "\x1blua"
+)
+
+// data to catch conversion errors
+const (
+	LUAC_DATA = "\x19\x93\r\n\x1a\n"
+	LUAC_INT  = 0x5678
+)
